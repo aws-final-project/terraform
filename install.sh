2a174ab69e60dd63d40f1e9cac4bff59147e5a6c
tod-easton <tod.easton.fv6o@statefarm.com>
2021-11-18 08:30:37 -0700

    update path in git clone for install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,3 @@ sudo yum update -y
 sudo yum install git -y
 sudo amazon-linux-extras install java-openjdk11 -y
 git clone https://github.com/aws-final-project/aws-final-project.git /home/ec2-user/aws-final-project
-
